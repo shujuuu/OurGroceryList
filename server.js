287bd530df9ccc18f647d656286d99d0691471ff
shujuuu <shuj_lin@yahoo.com>
2020-06-07 03:35:30 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,44 @@
+//Server-side code
+//HTTP
+const app = require("http").createServer(handler);
+const fs = require('fs');
+const url = require('url');
+app.listen(80);
+console.log("your server is running at port 80!")
+
+//requesting handler
+function handler(req, res) {
+    const parsedUrl = url.parse(req.url);
+    console.log("The Request is: " + parsedUrl.pathname);
+
+    fs.readFile(__dirname + parsedUrl.pathname,
+        function (err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading ' + parsedUrl.pathname);
+            }
+            res.writeHead(200);
+            res.end(data);
+        }
+    );
+}
+
+//WebSockets Communication
+const io = require("socket.io").listen(app);
+
+//Start Communication
+io.sockets.on("connection", socket => {
+    console.log('a new client has connected: ' + socket.id);
+
+    //receive from a client event
+    socket.on('add_item', data => {
+        console.log("new item from client: " + data);
+        const dataToClient = data;
+
+        //emit from server to everyone
+        socket.emit("receive_item", dataToClient);
+
+        //emit from server to everyone but the clients socket
+        // socket.broadcast.emit("receive_item", dataToClient);
+    })
+});
