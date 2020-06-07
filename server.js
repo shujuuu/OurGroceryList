c190a7a63eddc942591e7c41f0f5029cf810c887
shujuuu <shuj_lin@yahoo.com>
2020-06-07 13:27:28 -0400

    change /index to /

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 //Server-side code
-//HTTP
+//Create an HTTP server
 const app = require("http").createServer(handler);
 const fs = require('fs');
 const url = require('url');
-
-// app.listen(80);
-// console.log("your server is running at port 80!")
-
 const port = process.env.PORT || 3000
-
+const INDEX = '/index.html';
 app.listen(port, () => {
-    console.log("your server is running at port 3000!");
+    console.log("Server runs successfully! Check your shopping list at: http://localhost:3000");
 });
 
 //requesting handler
@@ -18,27 +14,27 @@ function handler(req, res) {
     const parsedUrl = url.parse(req.url);
     console.log("The Request is: " + parsedUrl.pathname);
 
-    fs.readFile(__dirname + parsedUrl.pathname,
-        function (err, data) {
+    if (parsedUrl.pathname === '/') {
+        const htmlPath = __dirname + parsedUrl.pathname + 'index.html'
+        fs.readFile(htmlPath, (err, data) => {
             if (err) {
                 res.writeHead(500);
-                return res.end('Error loading ' + parsedUrl.pathname);
+                return res.end('Error loading ' + htmlPath);
             }
             res.writeHead(200);
             res.end(data);
-        }
-    );
-    // res.statusCode = 200;
-    // res.setHeader('Content-Type', 'text/html');
-    // res.end('<h1>Hello World</h1>');
+        });
+    }
 }
 
-//WebSockets Communication
+//Create the WebSockets Communication
 const io = require("socket.io").listen(app);
-
+let totalClients = 0;
 //Start Communication
 io.sockets.on("connection", socket => {
+    totalClients++;
     console.log('a new client has connected: ' + socket.id);
+    console.log('total clients: ' + totalClients);
 
     //receive from a client event
     socket.on('add_item', data => {
@@ -46,9 +42,16 @@ io.sockets.on("connection", socket => {
         const dataToClient = data;
 
         //emit from server to everyone
-        // socket.emit("receive_item", dataToClient);
+        io.sockets.emit("receive_item", dataToClient);
 
-        //emit from server to everyone but the clients socket
-        socket.broadcast.emit("receive_item", dataToClient);
+        //emit from server to everyone but the sender
+        // socket.broadcast.emit("receive_item", dataToClient);
+    })
+
+    //when a client disconnects
+    socket.on('disconnect', () => {
+        totalClients--;
+        console.log('total users: ' + totalClients);
+        console.log('a client disconnects: ' + socket.id);
     })
 });
